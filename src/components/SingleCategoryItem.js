3b9b8571982a803517610f0ac0a68b97072c9670
kommwirt <dw@kommunikationsfachwirt.de>
2021-06-29 00:11:47 +0200

    Bug dixing

--- src/components/SingleCategoryItem.js
+++ src/components/SingleCategoryItem.js
@@ -11,7 +11,7 @@ const SingleCategoryItem = (props) => {
             <Link to={`/recipe/${props.data.idMeal}`}>
                 <div className="categories-single-item">
                     {/* <h2>{props.data.strMeal.slice(0,20)}</h2> */}
-                    <h2>{props.data.strMeal.split("with" [0])}</h2>
+                    <h2>{props.data.strMeal.split('with')[0]}</h2>
                     <img src={props.data.strMealThumb} alt="name"></img>
                 </div>
             </Link>
