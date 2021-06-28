590a51f9188e797c3e6b59a39b31e785cb5d101d
kommwirt <dw@kommunikationsfachwirt.de>
2021-06-28 23:05:21 +0200

    Category cards

--- src/components/SingleCategoryItem.js
+++ src/components/SingleCategoryItem.js
@@ -5,11 +5,13 @@ import { Link } from 'react-router-dom';
 
 const SingleCategoryItem = (props) => {
     console.log(props)
+    let words = props.data.strMeal.split(" " , 4)
     return (
         <Col xs="12" md="12" lg="6" className="div-radius">
             <Link to={`/recipe/${props.data.idMeal}`}>
                 <div className="categories-single-item">
-                    <h2>{props.data.strMeal.slice(0,20)}</h2>
+                    {/* <h2>{props.data.strMeal.slice(0,20)}</h2> */}
+                    <h2>{words.join(" ")}</h2>
                     <img src={props.data.strMealThumb} alt="name"></img>
                 </div>
             </Link>
