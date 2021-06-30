a818f87cd4a36832373a9dbc16615d8f4b3cbee0
kommwirt <dw@kommunikationsfachwirt.de>
2021-06-30 13:17:09 +0200

    Youtube link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
             <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
                 <img src="/img/insta.png" alt="" />
             </a>
-            <a href="https://youtu.be/gB2QgvtIVwY?t=55" target="_blank">
+            <a href="https://youtu.be/gB2QgvtIVwY?t=55" target="_blank" rel="noreferrer">
                 <img src="/img/youtube.png" alt="" />
             </a>
             <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
