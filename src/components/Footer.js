31b8b9d9da2fb2dd6e381dea99199c818cd4ba0e
webok21 <webok21@gmail.com>
2021-06-30 11:52:04 +0200

    social media links updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,15 @@
 import './../css/Footer.css'
-import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
         <div className="footer">
-            <a href="https://www.instagram.com" target="_blank">
+            <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
                 <img src="/img/insta.png" alt="" />
             </a>
-            <a href="https://www.youtube.com" target="_blank">
+            <a href="https://www.youtube.com" target="_blank" rel="noreferrer">
                 <img src="/img/youtube.png" alt="" />
             </a>
-            <a href="https://www.facebook.com" target="_blank">
+            <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
                 <img src="/img/fb.png" alt="" />
             </a>
         </div>
